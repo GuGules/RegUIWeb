be11368f61599a942ac1a6695007344ec1a432a0
Jules PILLOT <jules.pillot85@gmail.com>
2025-08-06 19:01:15 +0200

    ci: test for another job

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -10,22 +10,70 @@ import { items, start, end } from "@/app/lib/menubar_items";
 import { Menubar } from "primereact/menubar";
 import { TabView,TabPanel } from "primereact/tabview";
 import { Dropdown } from 'primereact/dropdown';
+import { InputText } from "primereact/inputtext";
+import { FloatLabel } from "primereact/floatlabel";
+import { InputSwitch } from "primereact/inputswitch";
+import { Button } from "primereact/button";
 
 export default function AdminPage(){
-
-    const SMTPConfig = ()=>{
-        return ( 
-            <>
-                <div></div>
-            </>
-        )
-    }
 
     const mailOptions = [
         { name: 'SMTP', value: 'SMTP' },
         { name: 'Resend', value: 'Resend' }
     ];
     const [emailMode, setEmailMode] = useState<string>('');
+    const [smtpHost, setSmtpHost] = useState<string>('');
+    const [smtpPort, setSmtpPort] = useState<string>('');
+    const [smtpUser, setSmtpUser] = useState<string>('');
+    const [smtpPassword, setSmtpPassword] = useState<string>('');
+    // Pour le switch, on utilise un boolean
+    const [smtpSecure, setSmtpSecure] = useState<boolean>(false);
+
+    const SMTPConfig = ()=>{
+        return ( 
+            <>
+                <div style={{height:'1rem'}}></div>
+                <FloatLabel>
+                    <label htmlFor="smtpHost">Hote SMTP</label>
+                    <InputText id="smtpHost" value={smtpHost} style={{width:'100%'}} onChange={(e) => setSmtpHost(e.target.value)} />
+                </FloatLabel>
+                <div style={{height:'1.3rem'}}></div>
+                <FloatLabel>
+                    <label htmlFor="smtpPort">Port SMTP</label>
+                    <InputText id="smtpPort" value={smtpPort} style={{width:'100%'}} onChange={(e) => setSmtpPort(e.target.value)} />
+                </FloatLabel>
+                <div style={{height:'1.3rem'}}></div>
+                <FloatLabel>
+                    <label htmlFor="smtpUser">Utilisateur</label>
+                    <InputText id="smtpUser" value={smtpUser} style={{width:'100%'}} onChange={(e) => setSmtpUser(e.target.value)} />
+                </FloatLabel>
+                <div style={{height:'1.3rem'}}></div>
+                <FloatLabel>
+                    <label htmlFor="smtpPassword">Mot de passe</label>
+                    <InputText id="smtpPassword" value={smtpPassword} style={{width:'100%'}} onChange={(e) => setSmtpPassword(e.target.value)} />
+                </FloatLabel>
+                <div style={{height:'1.3rem'}}></div>
+                <label htmlFor="smtpSecure">Connexion sécurisée : &nbsp;</label>
+                <InputSwitch checked={smtpSecure} onChange={(e) => setSmtpSecure(e.value)} />
+                <div style={{height:'1.3rem'}}></div>
+                <Button label="Tester la connexion" icon="pi pi-check" onClick={() => {}} />&nbsp;
+                <Button label="Enregistrer la configuration" icon="pi pi-save" onClick={() => {}} />
+            </>
+        )
+    }
+
+    const ResendConfig = ()=> {
+        return (
+            <>
+            Comes Later
+            </>
+        )
+    }
+
+
+
+
+
     useCheckSessionForAdmin();
 
     return(
@@ -36,7 +84,7 @@ export default function AdminPage(){
             <h1 className="text-2xl text-gray-950">Page de configuration</h1>
                 <TabView> 
                     <TabPanel header="&nbsp;Globale" leftIcon="pi pi-cog">
-                        
+                        <p>Comes later</p>
                     </TabPanel>
                     <TabPanel header="&nbsp;Envoi de mail" leftIcon="pi pi-envelope">
                         <label className="ml-2" htmlFor="emailModeSelector">Mode d'envoi de mail : {emailMode}</label>
@@ -44,7 +92,7 @@ export default function AdminPage(){
                         placeholder="Sélectionner un mode d'envoi de mail" className="w-full md:w-14rem" checkmark={true}  highlightOnSelect={false} />
                         <div style={{height:'1rem'}}></div>
                         {emailMode === 'SMTP' ? <SMTPConfig /> : null}
-                        {emailMode === 'Resend' ? <div>Resend selected</div> : null}
+                        {emailMode === 'Resend' ? <ResendConfig /> : null}
                     </TabPanel>
                 </TabView>
                 
