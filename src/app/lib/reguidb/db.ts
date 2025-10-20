fb2107dfb2c042759d9c89bbf5cfb5f464ea118a
Jules PILLOT <jules.pillot85@gmail.com>
2025-10-20 22:02:22 +0200

    fix: static variables

--- src/app/lib/reguidb/db.ts
+++ src/app/lib/reguidb/db.ts
@@ -7,7 +7,7 @@ export const pool = await mysql.createPool({
     port: parseInt(process.env.DB_PORT || '3306', 10),
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: 'regui',
+    database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: parseInt(process.env.DB_CONNECTION_LIMIT || '10', 10), // Ajuste selon tes besoins
     queueLimit: 0
